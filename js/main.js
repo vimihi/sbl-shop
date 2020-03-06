3dc0d0ffc0709e1c60e6b49b04a6134ee122e4c8
Phat Nguyen <vimihi3@gmail.com>
2020-03-06 22:02:52 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $('.sbl-feature-slider').slick({
+        arrows: true,
+        speed: 300,
+        slidesToShow: 3,
+        variableWidth: true,
+        infinite: true,
+        centerMode: true
+    });
+});
